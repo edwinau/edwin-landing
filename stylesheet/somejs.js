088e8c517b8469ccfd59193e70b771639739cfb7
edwinau@rogers.com <edwinau@rogers.com>
2023-01-28 22:36:33 -0500

    adding hamburger to desktop view

--- stylesheet/somejs.js
+++ stylesheet/somejs.js
@@ -220,3 +220,23 @@ function openTest() {
 }
 
 document.getElementById('btn').addEventListener('click', openTest);
+
+
+//for resumemobile movement
+function reveal() {
+  var reveals = document.querySelectorAll(".resumepdf");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
