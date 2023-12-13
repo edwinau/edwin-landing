d524fb879a1aa0ce9f49f567ac6d92cc3e491d43
edwinau@rogers.com <edwinau@rogers.com>
2023-12-12 23:25:48 -0500

    new web browser titles

--- stylesheet/somejs.js
+++ stylesheet/somejs.js
@@ -222,24 +222,6 @@ function openTest() {
 document.getElementById('btn').addEventListener('click', openTest);
 
 
-//for resumemobile movement
-function reveal() {
-  var reveals = document.querySelectorAll(".resumepdf");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
-
-window.addEventListener("scroll", reveal);
 
 // When the user clicks on div, open the popup
 function myFunction() {
